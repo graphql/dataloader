d0374ec0142b6f802e6e48a46313246ff96cada7
Lee Byron <lee@leebyron.com>
2019-11-13 10:25:01 -0800

    Use ReadonlyArray in promise result

--- src/index.d.ts
+++ src/index.d.ts
@@ -71,7 +71,7 @@ declare namespace DataLoader {
   // A Function, which when given an Array of keys, returns a Promise of an Array
   // of values or Errors.
   export type BatchLoadFn<K, V> = 
-    (keys: ReadonlyArray<K>) => PromiseLike<Array<V | Error>>;
+    (keys: ReadonlyArray<K>) => PromiseLike<ReadonlyArray<V | Error>>;
 
   // Optionally turn off batching or caching or provide a cache key function or a
   // custom cache instance.
