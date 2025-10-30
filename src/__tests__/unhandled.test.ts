6da3bbbaf647de1d6efbb6e8ff61158262f6698b
Nima Shahri <NShahri@users.noreply.github.com>
2025-10-30 15:55:20 +1300

    Implement custom handler for unhandled promise rejections in Jest tests

--- src/__tests__/unhandled.test.ts
+++ src/__tests__/unhandled.test.ts
@@ -7,22 +7,22 @@
 
 import DataLoader from '../index.ts';
 import { describe, it, expect, jest } from '@jest/globals';
-import process from 'node:process';
 
 describe('Unhandled rejections', () => {
   it('Not catching a primed error is an unhandled rejection', async () => {
-    //process.removeAllListeners('unhandledRejection');
     const handler = jest.fn();
-    process.on('unhandledRejection', handler); //detectOpenHandles
+    // @ts-expect-error need to use injected _onUnhandledRejection as the original process.on may have been overridden by Jest
+    global._onUnhandledRejection(handler); //detectOpenHandles
 
     const identityLoader = new DataLoader<number, number>(async keys => keys);
     identityLoader.prime(1, new Error('Error: 1'));
 
-    // Ignore result.
     const promise = identityLoader.load(1);
 
     await new Promise(resolve => setTimeout(resolve, 1000));
     expect(handler).toHaveBeenCalled();
+
+    // Prevent Jest from complaining about the unhandled rejection
     promise.catch(() => {});
   });
 });
